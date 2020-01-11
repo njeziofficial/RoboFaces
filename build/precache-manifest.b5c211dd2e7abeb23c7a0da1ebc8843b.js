31682c21f957aae8be953cad1279c08023dbc54f
njeziofficial <njezichigozie@yahoo.com>
2020-01-11 01:04:07 +0100

    Add files via upload

---
+++ build/precache-manifest.b5c211dd2e7abeb23c7a0da1ebc8843b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a8d205d09a926639bc17da067b8d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cd824f145a6f9edcd1bb",
+    "url": "/static/css/main.82be9628.chunk.css"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/static/js/2.3a77dedc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3a77dedc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd824f145a6f9edcd1bb",
+    "url": "/static/js/main.fd4ae184.chunk.js"
+  },
+  {
+    "revision": "233e64e2b074943728ef",
+    "url": "/static/js/runtime-main.cff0e806.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
